04b20bd6005643fa5fb7a7a9b743426b86071811
naftis <pyry.rouvila@gmail.com>
2024-08-20 01:05:51 +0300

    chore: move eslint to gateway directory
    currently does not work, need to debug

--- packages/gateway/eslint.config.mjs
+++ packages/gateway/eslint.config.mjs
@@ -4,12 +4,4 @@ import tseslint from 'typescript-eslint';
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
-  {
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
 );
