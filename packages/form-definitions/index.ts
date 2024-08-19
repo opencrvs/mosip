11fbf762a6be4d0223c20da13b7f09defcf4d7a9
naftis <pyry.rouvila@gmail.com>
2024-08-20 00:01:05 +0300

    feat: build mechanism for the form definitions
    placeholder values for now

--- packages/form-definitions/index.ts
+++ packages/form-definitions/index.ts
@@ -1,13 +1,34 @@
-export const formPopupButton = ({
-  url,
-}: {
-  /**
-   * URL to OpenCRVS-MOSIP Nid Authenticator (e.g. https://opencrvs-mosip-gateway.farajaland.opencrvs.org)
-   */
-  url: string;
-}) => {
+/**
+ * E-Signet popup button form definition
+ * @param url URL to OpenCRVS-MOSIP gateway (e.g. https://opencrvs-mosip-gateway.farajaland.opencrvs.org)
+ */
+export const popupButton = ({ url }: { url: string }) => {
   return {
+    name: "INFORMANT_AUTHENTICATION_POPUP_BUTTON",
     type: "POPUP_BUTTON",
     url,
   };
 };
+
+export const hidden = () => {
+  return {
+    name: "INFORMANT_PSUT_TOKEN",
+    type: "HIDDEN",
+  };
+};
+
+/**
+ * E-Signet popup button and hidden field form definitions
+ * @param url URL to OpenCRVS-MOSIP gateway (e.g. https://opencrvs-mosip-gateway.farajaland.opencrvs.org)
+ * @example
+ * ```
+ * [
+ *   // ...other fields
+ *   ...esignet({ url: "https://opencrvs-mosip-gateway.farajaland.opencrvs.org" })
+ * ]
+ * ```
+ */
+export const esignet = ({ url }: { url: string }) => [
+  popupButton({ url }),
+  hidden(),
+];
