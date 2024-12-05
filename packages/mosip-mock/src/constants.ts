20b6ca479efd5f88b9dcee8aef5f32acb80600cf
naftis <pyry.rouvila@gmail.com>
2024-12-05 15:50:25 +0200

    fix(mock): port is 2024_0_

--- packages/mosip-mock/src/constants.ts
+++ packages/mosip-mock/src/constants.ts
@@ -1,7 +1,7 @@
 import { cleanEnv, num, port, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
-  PORT: port({ default: 2024 }),
+  PORT: port({ default: 20240 }),
   HOST: str({ default: "0.0.0.0", devDefault: "localhost" }),
   OPENCRVS_MOSIP_SERVER_URL: str({
     devDefault: "http://localhost:2024/webhooks/mosip",
