31bbe14d5dc3689bf91de6dd83eb862c201e4636
naftis <pyry.rouvila@gmail.com>
2024-12-11 22:35:35 +0200

    fix: invalidate obviously fake NID's
    default countryconfig has 10char strict limit to nids so wanna conform to that

--- packages/mosip-mock/src/webhooks/deactivate-nid.ts
+++ packages/mosip-mock/src/webhooks/deactivate-nid.ts
@@ -9,16 +9,31 @@ export const deactivateNidHandler: RouteHandlerMethod = async (
     nid: string;
   };
 
-  // Fake validation logic for QA:
-  // * if the length is under 10 characters -> 404 Not Found (the NID wasn't found in the system)
-  // * if the NID is over 10 characters     -> 409 Conflict  (the NID has already been deactivated)
-  const nidNotFound = nid.length < 10;
-  if (nidNotFound) {
+  // Fake validation logic for if "NID is not found" or "NID is already deactivated"
+  const notFoundNids = [
+    "0000000000",
+    "1111111111",
+    "2222222222",
+    "3333333333",
+    "4444444444",
+  ];
+  if (notFoundNids.includes(nid)) {
+    sendEmail(`NID not found for ${nid}`, `NID not found: ${nid}`);
     return reply.status(404).send();
   }
 
-  const nidAlreadyDeactivated = nid.length > 10;
-  if (nidAlreadyDeactivated) {
+  const alreadyDeactivatedNids = [
+    "5555555555",
+    "6666666666",
+    "7777777777",
+    "8888888888",
+    "9999999999",
+  ];
+  if (alreadyDeactivatedNids.includes(nid)) {
+    sendEmail(
+      `NID already deactivated for ${nid}`,
+      `NID conflict, already deactivated: ${nid}`
+    );
     return reply.status(409).send();
   }
 
