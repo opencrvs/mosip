0e25fa62c0d8a5e551bb704c109fdbb5b96c356a
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2024-12-17 19:22:36 +0600

    chore: id reader config

--- packages/country-config/src/forms.ts
+++ packages/country-config/src/forms.ts
@@ -5,21 +5,21 @@
  */
 export const popupButton = ({
   /** URL to OpenCRVS-MOSIP gateway (e.g. https://opencrvs-mosip-gateway.farajaland.opencrvs.org) */
-  url,
+  url
 }: {
   url: string;
 }) => {
   return {
-    name: "INFORMANT_AUTHENTICATION_POPUP_BUTTON",
-    type: "POPUP_BUTTON",
-    url,
+    name: 'INFORMANT_AUTHENTICATION_POPUP_BUTTON',
+    type: 'POPUP_BUTTON',
+    url
   };
 };
 
 export const hidden = () => {
   return {
-    name: "INFORMANT_PSUT_TOKEN",
-    type: "HIDDEN",
+    name: 'INFORMANT_PSUT_TOKEN',
+    type: 'HIDDEN'
   };
 };
 
@@ -33,8 +33,51 @@ export const hidden = () => {
  * ]
  * ```
  */
+
+export const idReader = ({ url }) => {
+  return {
+    name: 'informantIDReader',
+    type: 'ID_READER',
+    readers: [
+      {
+        type: 'qr',
+        labels: {
+          button: {
+            id: 'id.qr.button.label',
+            defaultMessage: 'Scan ID QR code'
+          },
+          scannerDialogSupportingCopy: {
+            id: 'id.qr.scanner.supportingCopy',
+            defaultMessage:
+              "Place the Notifier's ID card in front of the camera."
+          },
+          tutorial: {
+            cameraCleanliness: {
+              id: 'id.qr.tutorial.cameraCleanliness',
+              defaultMessage: 'Ensure your camera is clean and functional.'
+            },
+            distance: {
+              id: 'id.qr.tutorial.distance',
+              defaultMessage:
+                'Hold the device steadily 6-12 inches away from the QR code.'
+            },
+            lightBalance: {
+              id: 'id.qr.tutorial.lightBalance',
+              defaultMessage:
+                'Ensure the QR code is well-lit and not damaged or blurry.'
+            }
+          }
+        }
+      }
+      // {
+      //   type: 'e-signet',
+      //   ...otherProperties
+      // }
+    ]
+  };
+};
 export const esignet = ({
-  url,
+  url
 }: {
   /** URL to OpenCRVS-MOSIP gateway (e.g. https://opencrvs-mosip-gateway.farajaland.opencrvs.org) */
   url: string;
