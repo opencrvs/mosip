1423492519b9c127b1e3f0bc2603668463d886c0
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2025-02-03 14:19:47 +0600

    chore: update verified logic to checking on stubbed ids

--- packages/mosip-api/src/routes/event-review.ts
+++ packages/mosip-api/src/routes/event-review.ts
@@ -1,16 +1,32 @@
 import { FastifyRequest, FastifyReply } from "fastify";
-import { getComposition, getInformantType } from "../types/fhir";
+import {
+  getComposition,
+  getInformantNationalId,
+  getInformantType,
+} from "../types/fhir";
 import { updateField } from "../opencrvs-api";
 
 type OpenCRVSRequest = FastifyRequest<{
   Body: fhir3.Bundle;
 }>;
 
+const stubValidNIDs = [
+  "1204837295",
+  "1210563847",
+  "1223948576",
+  "1238475062",
+  "1249583720",
+  "1256074839",
+  "1263849205",
+  "1275093846",
+  "1283749502",
+  "1295067384",
+];
 export const reviewEventHandler = async (
   request: OpenCRVSRequest,
   reply: FastifyReply,
 ) => {
-  const informantType = getInformantType(request.body);
+  const informantNationalID = getInformantNationalId(request.body);
   const { id: eventId } = getComposition(request.body);
 
   if (!request.headers.authorization) {
@@ -25,7 +41,7 @@ export const reviewEventHandler = async (
   }
 
   // Initial test of the verification, we will verify only other informants than mother and father
-  if (informantType !== "mother" && informantType !== "father") {
+  if (!stubValidNIDs.includes(informantNationalID)) {
     await updateField(
       eventId,
       `birth.informant.informant-view-group.verified`,
