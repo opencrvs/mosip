a7f4b45cfab5927ac4c8b20d67a1c4951ad22b3e
naftis <pyry.rouvila@gmail.com>
2025-02-27 10:43:24 +0200

    fix: is thumbprint to true

--- packages/mosip-api/src/crypto/legacy-mediator/crypto-constants.ts
+++ packages/mosip-api/src/crypto/legacy-mediator/crypto-constants.ts
@@ -9,4 +9,4 @@ export const GCM_TAG_LENGTH = 16;
 export const THUMBPRINT_LENGTH = 32;
 
 /** @TODO: Figure out if this is actually ever configured, as earlier it was from the environment? */
-export const IS_THUMBPRINT = false;
+export const IS_THUMBPRINT = true;
