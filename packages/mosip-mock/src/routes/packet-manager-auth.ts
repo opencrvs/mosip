1ae7b24140c6f6c4f739ab3a5afbf2587dcf8736
naftis <pyry.rouvila@gmail.com>
2025-03-19 14:29:44 +0200

    chore: amend mock responses based on Bevolvs Postman

--- packages/mosip-mock/src/routes/packet-manager-auth.ts
+++ packages/mosip-mock/src/routes/packet-manager-auth.ts
@@ -4,7 +4,21 @@ export const packetManagerAuthHandler: RouteHandlerMethod = async (
   request,
   reply,
 ) => {
-  return reply
-    .status(200)
-    .send({ todo: "TODO: Figure out the response from Bevolv" });
+  const token = "some-token";
+  reply.header(
+    "set-cookie",
+    `Authorization=${token}; Path=/; HttpOnly; Secure; SameSite=Strict`,
+  );
+
+  return reply.status(200).send({
+    id: "string",
+    version: "string",
+    responsetime: new Date().toISOString(),
+    metadata: null,
+    response: {
+      status: "Success",
+      message: "Clientid and Token combination had been validated successfully",
+    },
+    errors: null,
+  });
 };
--- packages/mosip-mock/src/routes/packet-manager-create.ts
+++ packages/mosip-mock/src/routes/packet-manager-create.ts
@@ -43,9 +43,12 @@ const sendNid = async ({
   return response.text();
 };
 
-type OpenCRVSBirthEvent = {
-  id: string;
-  trackingId: string;
+type CrvsNewRequest = {
+  request: {
+    id: string;
+    refId: string;
+    process: "CRVS_NEW";
+  };
 };
 
 /** Handles the births coming from OpenCRVS */
@@ -53,18 +56,67 @@ export const packetManagerCreateHandler: RouteHandlerMethod = async (
   request,
   reply,
 ) => {
-  const { token, event } = request.body as {
-    token: string;
-    event: OpenCRVSBirthEvent;
-  };
+  const {
+    request: { id, refId },
+  } = request.body as CrvsNewRequest;
 
-  sendNid({ token, eventId: event.id, trackingId: event.trackingId }).catch(
-    (e) => {
-      console.error(e);
-    },
-  );
+  // @TODO: Amend when we know how the WebSub responds
+
+  // sendNid({ token, eventId: event.id, trackingId: event.trackingId }).catch(
+  //   (e) => {
+  //     console.error(e);
+  //   },
+  // );
 
   return reply.status(202).send({
-    aid: createAid(),
+    id: "mosip.registration.packet.writer",
+    version: "v1",
+    responsetime: new Date().toISOString(),
+    metadata: null,
+    response: [
+      {
+        id,
+        packetName: "111111112_evidence",
+        source: "OPENCRVS",
+        process: "CRVS_NEW",
+        refId,
+        schemaVersion: "0.1",
+        signature:
+          "Pjss9ng-js3eZZ6eUxOjjzNtKTyAzwWNI7qun8cN2UAEIS-rV3YzionqJId1WBdNFoistFPlrqrGnfI2xaX5MY95ttMWYzw89JyAWi-VeDaYPVFWEvcLEUTU68e0uBMXFG6D15_zjiSrn9OW8pRMngUuusOaIlmGh2BwiM1NwmLbSTRHpJ0LkC69SiWQcA5igTR5ihuJtWcygiBiEX48LOaFvOkhyoShnYGlY8_gW2Ew_Z26EnJ7vP2y_ODZYqzWTRTHTDivsKQHyWdBczwGium6aug6A0H0lPSqxTXJslKylsotbA79Bv6gUviTLVjdglLdHdxIWNaVfmKeHiCQ9w",
+        encryptedHash: "Cly6hXkKx6TbqwjH3Frm9DZsCPjmCnB6aVKd_8sGRm0",
+        providerName: "PacketWriterImpl",
+        providerVersion: "v1.0",
+        creationDate: new Date().toISOString(),
+      },
+      {
+        id,
+        packetName: "111111112_optional",
+        source: "OPENCRVS",
+        process: "CRVS_NEW",
+        refId,
+        schemaVersion: "0.1",
+        signature:
+          "Pjss9ng-js3eZZ6eUxOjjzNtKTyAzwWNI7qun8cN2UAEIS-rV3YzionqJId1WBdNFoistFPlrqrGnfI2xaX5MY95ttMWYzw89JyAWi-VeDaYPVFWEvcLEUTU68e0uBMXFG6D15_zjiSrn9OW8pRMngUuusOaIlmGh2BwiM1NwmLbSTRHpJ0LkC69SiWQcA5igTR5ihuJtWcygiBiEX48LOaFvOkhyoShnYGlY8_gW2Ew_Z26EnJ7vP2y_ODZYqzWTRTHTDivsKQHyWdBczwGium6aug6A0H0lPSqxTXJslKylsotbA79Bv6gUviTLVjdglLdHdxIWNaVfmKeHiCQ9w",
+        encryptedHash: "pb0yrVpnz8QwLFI2ghVXs4hPkeGivnviph7SBpjJTxU",
+        providerName: "PacketWriterImpl",
+        providerVersion: "v1.0",
+        creationDate: new Date().toISOString(),
+      },
+      {
+        id,
+        packetName: "111111112_id",
+        source: "OPENCRVS",
+        process: "CRVS_NEW",
+        refId,
+        schemaVersion: "0.1",
+        signature:
+          "t_ShCVyVq_pro87V95AxympFG4gvnFjPHD466pgeSsRtM9KIaAH5GGOMc8jS_523wbWPCcKb-xpCPom1wYNQ5MvN_SkYitoflBC9UWRwdoZlCBAJFeaelyplFe5KmOIU28qZggoXq6fDbJXY44se72l8sGCU-hqtiz8wUFeh159D8ZN8YXs_RFnt7mmkkbro5P1nP7ZycgJwV_nbl8Z_96dvuhCnWZ06AwiSvGx4eZYkOJPcZ-1VJWA59RAsm6ez28dJV4l81Ixl1IJPzdjD_bsHe1agwOxoLqiMvMyBoCTvCy30YAtbdtmQPzKX9eohj68EZFhzT73gEi3MQMFgPA",
+        encryptedHash: "fZcsf60DnFujzr_3CGfgkXy3t7Z1nQ4MRkxVvfFoslo",
+        providerName: "PacketWriterImpl",
+        providerVersion: "v1.0",
+        creationDate: new Date().toISOString(),
+      },
+    ],
+    errors: [],
   });
 };
