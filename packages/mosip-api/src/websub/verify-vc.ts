ba3de77efb42a8d11b1aca48868441bb217bfc5b
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2025-05-07 20:10:21 +0600

    fix: wip

--- packages/mosip-api/src/websub/verify-vc.ts
+++ packages/mosip-api/src/websub/verify-vc.ts
@@ -1,7 +1,49 @@
-import vc from "@digitalbazaar/vc";
+import * as vc from "@digitalbazaar/vc";
+import { z } from "zod";
 import { Ed25519Signature2020 } from "@digitalbazaar/ed25519-signature-2020";
 import { Ed25519VerificationKey2020 } from "@digitalbazaar/ed25519-verification-key-2020";
 import { securityLoader } from "@digitalbazaar/security-document-loader";
+
+const BirthSubject = z.object({
+  birthCertificateNumber: z.string(),
+  VID: z.string(),
+  id: z.string().url(),
+  vcVer: z.literal("VC-V1"),
+});
+
+const DeathSubject = z.object({
+  id: z.string().url(),
+  vcVer: z.literal("VC-V1"),
+});
+
+export const isBirthSubject = (
+  subject: z.infer<typeof BirthSubject> | z.infer<typeof DeathSubject>,
+): subject is z.infer<typeof BirthSubject> => {
+  return "birthCertificateNumber" in subject && "VID" in subject;
+};
+
+export const MOSIPVerifiableCredential = z.object({
+  issuanceDate: z.string().datetime(),
+  credentialSubject: z.union([BirthSubject, DeathSubject]),
+  id: z.string().url(),
+  proof: z.object({
+    type: z.string(),
+    created: z.string().datetime(),
+    proofPurpose: z.string(),
+    verificationMethod: z.string().url(),
+    jws: z.string(),
+  }),
+  type: z.tuple([
+    z.literal("VerifiableCredential"),
+    z.literal("MOSIPVerifiableCredential"),
+  ]),
+  "@context": z.tuple([
+    z.literal("https://www.w3.org/2018/credentials/v1"),
+    z.string().endsWith("/.well-known/mosip-context.json"),
+    z.object({ sec: z.literal("https://w3id.org/security#") }),
+  ]),
+  issuer: z.string().url(),
+});
 
 export const verifyCredentialOrThrow = async (
   credential: any, // you can run it through your Zod schema first if you want
@@ -9,15 +51,21 @@ export const verifyCredentialOrThrow = async (
 ) => {
   const { verificationMethod } = credential.proof;
 
-  if (!allowList.includes(verificationMethod)) {
-    throw new Error("❌ Verification method not allowed");
-  }
+  // if (!allowList.includes(verificationMethod)) {
+  //   throw new Error("❌ Verification method not allowed");
+  // }
 
-  // Load public key (usually you'd fetch this from verificationMethod URI or a DID)
+  const publicKey =
+    "-----BEGIN PUBLIC KEY-----\r\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAw34EhsyQFAbB+jideCNO\r\n2HXAMQDR4NnwsVNQVcV8OxieMhi1pSreJZ8dt/MsgmdI3AW53WQ4ltXiZwdQ1Q/L\r\nm2t2kzFNAY57o8KG/V9sQIc5/oKH+G2y3+gotR+TaWxAg3RvSPG6X2KMSpKA4UB4\r\n+JVuqQNNZLdzGwVCf5+SMvjiamOX5xRnyyjhPJGKloykOIfmKaDRrXmV5QX5Lapk\r\nmy0GoAWIB96JpznDOFQlRMVGMKXr2crlxeHPBjiEm70ZnFpIGaacxCaFnuxWcCeb\r\nIoPufZaF7CkwVoSfKNltztsAgQkDUKO/kZ3hlxjj6do1f35Ow8mxU/RnSrq3mgFW\r\nWQIDAQAB\r\n-----END PUBLIC KEY-----";
+  const keyPair = await Ed25519VerificationKey2020.generate({
+    id: '"https://dev-api.identity.gov.to/.well-known/public-key.json"',
+    controller: "https://dev-api.identity.gov.to/.well-known/controller.json",
+    publicKeyPem: publicKey,
+  });
   const key = await Ed25519VerificationKey2020.from({
     id: verificationMethod,
     controller: credential.issuer,
-    publicKeyMultibase: "<your-public-key-multibase-here>", // Replace with actual key
+    publicKeyMultibase: keyPair.publicKeyMultibase,
   });
 
   const suite = new Ed25519Signature2020({ key });
@@ -30,6 +78,7 @@ export const verifyCredentialOrThrow = async (
     documentLoader,
   });
 
+  console.log("result", result);
   if (!result.verified) {
     throw new Error("❌ VC verification failed");
   }
