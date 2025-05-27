7d3ac12347ef6e412cda5455d5ee6ff18ac5a4ef
naftis <pyry.rouvila@gmail.com>
2025-05-27 13:41:13 +0300

    feat: restrict scopes who can debug the db

--- packages/mosip-api/src/routes/debug-sqlite.ts
+++ packages/mosip-api/src/routes/debug-sqlite.ts
@@ -1,7 +1,45 @@
 import { FastifyReply, FastifyRequest } from "fastify";
 import { getAllTransactions, getTransactionAndDiscard } from "../database";
 
-export const getAllTransactionsHandler = async () => {
+interface AuthenticatedUser {
+  scope: string[];
+}
+
+/**
+ * Allow listing transactions for users that have the search scope.
+ *
+ * Rationale:
+ * - Users with this scope would be able to see record UUID's and registration numbers in the UI anyway.
+ */
+const isAllowedToSearch = (scope: string[]) => {
+  return scope.includes("search.birth") && scope.includes("search.death");
+};
+
+/**
+ * Allow deleting transactions for users that have `record.reject-registration` scope.
+ *
+ * Rationale:
+ * - This should be accompanied with a `rejectRegistration` call via Postman which requires this scope.
+ */
+const isAllowedToDelete = (scope: string[]) => {
+  return (
+    isAllowedToSearch(scope) && scope.includes("record.reject-registration")
+  );
+};
+
+export const getAllTransactionsHandler = async (
+  request: FastifyRequest,
+  reply: FastifyReply,
+) => {
+  const { scope } = request.user as AuthenticatedUser;
+
+  if (!isAllowedToSearch(scope)) {
+    console.log(scope);
+    return reply.status(403).send({
+      error: "You do not have permission to access this resource.",
+    });
+  }
+
   return getAllTransactions();
 };
 
@@ -13,6 +51,14 @@ export const deleteTransactionHandler = async (
   request: DeleteTransactionRequest,
   reply: FastifyReply,
 ) => {
+  const { scope } = request.user as AuthenticatedUser;
+
+  if (!isAllowedToDelete(scope)) {
+    return reply.status(403).send({
+      error: "You do not have permission to access this resource.",
+    });
+  }
+
   const { id } = request.params;
 
   try {
