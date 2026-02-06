f13ae2c915a1b2dd3bc42cd6ab21dd5b47e5f734
naftis <pyry.rouvila@gmail.com>
2026-02-06 14:55:34 +0200

    disable verifying on mosip connect

--- packages/mosip-api/src/routes/verify.ts
+++ packages/mosip-api/src/routes/verify.ts
@@ -1,6 +1,6 @@
 import { DateValue, NameFieldValue, TextValue } from "@opencrvs/toolkit/events";
 import { FastifyReply, FastifyRequest } from "fastify";
-import { verifyNid } from "../mosip-api";
+// import { verifyNid } from "../mosip-api";
 import { z } from "zod";
 
 export const VerifySchema = z.object({
@@ -20,27 +20,29 @@ export const verifyHandler = async (
   request: OpenCRVSRequest,
   reply: FastifyReply,
 ) => {
-  const {
-    response: { authStatus },
-  } = await verifyNid({
-    nid: request.body.nid,
-    dob: request.body.dob.replaceAll("-", "/"),
-    name: [
-      {
-        language: "eng",
-        value: `${request.body.name.firstname} ${request.body.name.surname}`,
-      },
-    ],
-    gender: request.body.gender
-      ? [{ language: "eng", value: request.body.gender }]
-      : undefined,
-  });
+  // const {
+  //   response: { authStatus },
+  // } = await verifyNid({
+  //   nid: request.body.nid,
+  //   dob: request.body.dob.replaceAll("-", "/"),
+  //   name: [
+  //     {
+  //       language: "eng",
+  //       value: `${request.body.name.firstname} ${request.body.name.surname}`,
+  //     },
+  //   ],
+  //   gender: request.body.gender
+  //     ? [{ language: "eng", value: request.body.gender }]
+  //     : undefined,
+  // });
 
-  const transactionId = request.body.transactionId;
+  // const transactionId = request.body.transactionId;
 
-  if (transactionId) {
-    request.log.info({ transactionId, authStatus });
-  }
+  // if (transactionId) {
+  //   request.log.info({ transactionId, authStatus });
+  // }
 
-  return reply.code(200).send(authStatus ? "verified" : "failed");
+  // return reply.code(200).send(authStatus ? "verified" : "failed");
+
+  return reply.code(200).send("verified");
 };
